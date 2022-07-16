fb94d5383e0221c61cc820a61d7e4365906922b0
Eduardo Freire <dudufreiresantos@gmail.com>
2022-07-16 10:41:13 -0300

    added medium difficulty

--- src/enemy.s
+++ src/enemy.s
@@ -30,8 +30,120 @@ GET_EASY_OUTPUT:
   ret
 
 GET_MEDIUM_OUTPUT:
+  addi sp, sp, -4
+  sw ra, 0(sp)
 
-  xori s4, s4, 1 # Change Turns
+  # currentPlayer = (s4 == 0) ? 1:-1
+  li t0, -2
+  mul t0, t0, s4 
+  addi t0, t0, 1 
+  la t1, currentPlayer
+  sb t0, 0(t1)
+
+  # t0 = i = 0
+  li t0, 0
+
+LOOP1_GET_MEDIUM_OUTPUT:
+  li t1, 9
+  bge t0, t1, END_LOOP1_GET_MEDIUM_OUTPUT
+
+  # if (BOARD[i] != 0) continue
+  la t1, BOARD 
+  add t1, t1, t0
+  lb t2, 0(t1)
+  bnez t2, CONTINUE_LOOP1_GET_MEDIUM_OUTPUT
+
+  # BOARD[i] = currentPlayer
+  la t2, currentPlayer
+  lb t2, 0(t2)
+  sb t2, 0(t1)
+
+  addi sp, sp, -4
+  sw t0, 0(sp)
+
+  jal checkEnd 
+
+  lw t0, 0(sp)
+  addi sp, sp, 4
+
+  # BOARD[i] = 0
+  la t1, BOARD 
+  add t1, t1, t0
+  sb zero, 0(t1)
+
+  # if (checkEnd() != currentPlayer) continue
+  la t1, currentPlayer
+  lb t1, 0(t1)
+  bne a0, t1, CONTINUE_LOOP1_GET_MEDIUM_OUTPUT
+
+  mv a0, t0 
+  jal makeMove 
+  jal END_GET_MEDIUM_OUTPUT
+
+CONTINUE_LOOP1_GET_MEDIUM_OUTPUT:
+  addi t0, t0, 1
+  j LOOP1_GET_MEDIUM_OUTPUT
+
+END_LOOP1_GET_MEDIUM_OUTPUT:
+  # t0 = i = 0
+  li t0, 0
+
+LOOP2_GET_MEDIUM_OUTPUT:
+  li t1, 9
+  bge t0, t1, END_LOOP2_GET_MEDIUM_OUTPUT
+
+  # if (BOARD[i] != 0) continue
+  la t1, BOARD 
+  add t1, t1, t0
+  lb t2, 0(t1)
+  bnez t2, CONTINUE_LOOP2_GET_MEDIUM_OUTPUT
+
+  # BOARD[i] = -currentPlayer
+  la t2, currentPlayer
+  lb t2, 0(t2)
+  li t3, -1
+  mul t2, t2, t3
+  sb t2, 0(t1)
+
+  addi sp, sp, -4
+  sw t0, 0(sp)
+
+  jal checkEnd 
+
+  lw t0, 0(sp)
+  addi sp, sp, 4
+
+  # BOARD[i] = 0
+  la t1, BOARD 
+  add t1, t1, t0
+  sb zero, 0(t1)
+
+  # if (checkEnd() != -currentPlayer) continue
+  la t1, currentPlayer
+  lb t1, 0(t1)
+  li t2, -1
+  mul t1, t1, t2
+  bne a0, t1, CONTINUE_LOOP2_GET_MEDIUM_OUTPUT
+
+  mv a0, t0 
+  jal makeMove 
+  jal END_GET_MEDIUM_OUTPUT
+
+CONTINUE_LOOP2_GET_MEDIUM_OUTPUT:
+  addi t0, t0, 1
+  j LOOP2_GET_MEDIUM_OUTPUT
+
+END_LOOP2_GET_MEDIUM_OUTPUT:
+  jal GET_EASY_OUTPUT
+  lw ra, 0(sp)
+  addi sp, sp, 4
+  ret
+
+END_GET_MEDIUM_OUTPUT:
+  lw ra, 0(sp)
+  addi sp, sp, 4
+
+  xori s4, s4, 1
   ret
 
 GET_HARD_OUTPUT:
@@ -39,7 +151,6 @@ GET_HARD_OUTPUT:
   li t0, -2
   mul t0, t0, s4 
   addi t0, t0, 1 
-
   la t1, currentPlayer
   sb t0, 0(t1)
 
